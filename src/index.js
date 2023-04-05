31b51f7091016abdbb7d136f000ec89183d38f0f
Kris Thornton <Kristhornton@ymail.com>
2023-04-05 07:32:23 -0700

    Grabbed items from HTML in JS to use for event listeners.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './less/index.less'
 
 // Your code goes here!
+// BACKGROUND
+const mainbackground = document.querySelector('.container')
+console.log(mainbackground)
+
+// H1 HEADING
+const logoHeading = document.querySelector('.logo-heading');
+console.log(logoHeading)
+
+// NAVAGATION BAR
+const navA = document.querySelectorAll('nav a');
+console.log(navA)
+
+// MAIN IMAGE
+const newImage = document.querySelector('.intro img');
+console.log(newImage)
+
+// H2 HEADINGS
+const newH2 = document.querySelector('.intro h2');
+console.log(newH2)
+
+// ARTICLE TEXT
+const newText = document.querySelectorAll('.text-content');
+console.log(newText)
+
+// ARTICLE IMAGES
+const newImageContent = document.querySelectorAll('.img-content');
+console.log(newImageContent)
+
+// --------------------------------
+// EVENT LISTENERS
+// --------------------------------
+
+
+
